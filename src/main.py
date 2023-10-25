61059d44bf2c6e75f840667820daf31e34b0ef78
Aurore <aurore.bozec@epita.fr>
2023-10-25 21:32:20 +0200

    ->ban feature done

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import random
 from discord.ext import commands
 import discord
 
@@ -10,7 +11,7 @@ bot = commands.Bot(
     intents = intents # Set up basic permissions
 )
 
-bot.author_id = 0000000  # Change to your discord id
+bot.author_id = 642493596653584426  # Change to your discord id
 
 @bot.event
 async def on_ready():  # When the bot is ready
@@ -21,5 +22,46 @@ async def on_ready():  # When the bot is ready
 async def pong(ctx):
     await ctx.send('pong')
 
-token = "<MY_TOKEN>"
+@bot.command()
+async def name(ctx, message):
+    await ctx.send(message.author)
+
+@bot.command()
+async def d6(ctx):
+    number= random.randint(1,6)
+    await ctx.send(number)
+
+@bot.event
+async def on_message(message):
+    #Check that the message meet requirements
+    if message.content == "Salut tout le monde":
+        await message.channel.send(f"Bienvenue à {message.author.mention} sur le serveur discord ! N'oublie pas de faire !regles") 
+
+### Administration
+
+@bot.command()
+async def admin(ctx, member : discord.Member):
+    # Does the role Admin already exist?
+    role_admin = discord.utils.get(ctx.guild.role, name='Admin')
+    if not role_admin : 
+        #Create the role admin
+        await ctx.guild.create_role(name='Admin', permissions=discord.Permissions.all(), mentionable=True)
+    member.add_role("Admin")
+
+@bot.command()
+async def ban(ctx, member : discord.Member, reason : None):
+    funny_catchphrase = [ "Time to take the exit stage left, my friend!", "We're about to hit the 'eject' button on this one.", "This is your cue for a dramatic exit, à la reality TV.", "Consider this your free subscription to 'Outta Here' magazine", "You've officially been 'disinvited' to the party." ]
+    if member == None or member == ctx.message.author:
+        #Check that the ban is valid
+        await ctx.channel.send("You cannot ban yourself")
+    if reason == None:
+        #Check that the ban have a valid reason and if not choose a random reason in the list
+        number= random.randint(0, len(funny_catchphrase))
+        reason = funny_catchphrase[number]
+    await member.send(f"You have been banned from {ctx.guild.name} for {reason}")
+    #Precise in the channel that the member is banned
+    await ctx.channel.send(f"{member} is banned!")
+    
+
+token = "MTE2Njc4NDMwNDA5Mzg3MjE2OQ.GVap05.lIhfrmFgPKGEkcOZ_ufgGM2l_wMXmgV4T_HgzY"
 bot.run(token)  # Starts the bot
